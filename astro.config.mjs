d0931dedda5e3756560799233a5a15d104b909b5
kwassef <karimwassef4@gmail.com>
2025-11-29 16:44:09 -0500

    added hours section

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
+const isProd = import.meta.env.PROD;
+
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: isProd ? cloudflare() : undefined,
+  output: isProd ? undefined : "static",
 });
